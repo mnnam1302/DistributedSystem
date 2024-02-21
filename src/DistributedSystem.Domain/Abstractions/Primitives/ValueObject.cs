c4c944f8d4479de0c10c8fe8182db63fad1edef0
mnnam1302 <mnnam1302>
2024-02-21 16:24:06 +0700

    feat: build interface RepositotyBase and UnitOfWork and Entities, Aggregate root, Value object in Abstractions

---
+++ src/DistributedSystem.Domain/Abstractions/Primitives/ValueObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace DistributedSystem.Domain.Abstractions.Primitives
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetAtomicValues();
+
+        // Compare with other value object
+        public bool Equals(ValueObject? other)
+        {
+            return other is not null && ValuesAreEqual(other);
+        }
+
+        // Compare with other object
+        public override bool Equals(object? obj)
+        {
+            return obj is ValueObject other && ValuesAreEqual(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetAtomicValues()
+                .Aggregate(
+                    default(int),
+                    HashCode.Combine);
+        }
+
+        private bool ValuesAreEqual(ValueObject other)
+        {
+            return GetAtomicValues().SequenceEqual(other.GetAtomicValues());
+        }
+    }
+}
