13a797db2723d093eea7a37a307b9cbdde8eeb70
mnnam1302 <mnnam1302>
2024-02-24 10:32:41 +0700

    feat: configure JWT and middleware for authentication

---
+++ src/DistributedSystem.API/DependencyInjection/Extensions/JwtExtensions.cs
@@ -0,0 +1,61 @@
+﻿using DistributedSystem.Infrastructure.DependencyInjection.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace DistributedSystem.API.DependencyInjection.Extensions
+{
+    public static class JwtExtensions
+    {
+        public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                JwtOptions jwtOptions = new JwtOptions();
+                configuration.GetSection(nameof(JwtOptions)).Bind(jwtOptions);
+
+                /**
+                 * Store the JWT in the AuthenticationProperties allows you to retrieve it from the HttpContext at any time.
+                 * public async Task<IActionResult> SomeAction()
+                 * {
+                 *  using Microsoft.AspNetCore.Authentication;
+                 *  var accessToken = await HttpContext.GetTokenAsync("access_token");
+                 *  //...
+                 * }
+                 */
+                options.SaveToken = true; // Save token into AuthenticationProperties
+
+                //var Key = Encoding.UTF8.GetBytes("7jCDPbBkeW8asPxdIc3jRddWpB7l63fh"); // On production
+                var Key = Encoding.UTF8.GetBytes(jwtOptions.Secret);
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false, // on production, set to true
+                    ValidateAudience = false, // on production, set to true
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtOptions.Issuer,
+                    ValidAudience = jwtOptions.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Key),
+                    ClockSkew = TimeSpan.Zero
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                        {
+                            context.Response.Headers.Add("IS-TOKEN-EXPIRED", "true");
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        }
+    }
+}
