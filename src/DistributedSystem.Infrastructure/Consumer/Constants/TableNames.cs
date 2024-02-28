9835fa098218551216863577c5450b68c6b0f749
mnnam1302 <mnnam1302>
2024-02-28 15:50:58 +0700

    feat: implement Idempotent Pattern for Create Product at Infrastructure, particular at MongoDB

--- src/DistributedSystem.Infrastructure/Consumer/Constants/TableNames.cs
+++ src/DistributedSystem.Infrastructure/Consumer/Constants/TableNames.cs
@@ -4,6 +4,8 @@
     {
         // *********** Singular Noun ***********
         internal const string Product = nameof(Product);
+        internal const string Event = nameof(Event);
+
         internal const string Order = nameof(Order);
         internal const string Customer = nameof(Customer);
         internal const string User = nameof(User);
